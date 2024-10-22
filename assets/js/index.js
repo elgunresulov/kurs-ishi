c63569cd4d24412fe793915fc47612050141b0c4
elgunresulov <elgun.resulov2005@mail.ru>
2024-10-22 12:42:10 +0400

    kurs isi

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+//mobile sector
+const navbarBtn=document.querySelector(".fa-bars") 
+const mobileMenyu=document.querySelector(".mobile-menyu")
+
+navbarBtn.addEventListener("click", function(){
+    mobileMenyu.classList.add(".mobile-menyu-acan")
+})
